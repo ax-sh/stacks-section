98a39b3dd3c03d9f3cc1cdab989e7284b652a5a3
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-08-11 03:12:37 +0545

    Add SearchInput component to app
    This commit introduces a new SearchInput component that is used to allow search functionality in the application. A SearchIcon component is also added and used in the SearchInput component. The new component is used in the app/page.tsx, replacing the placeholder search config text. The SearchInput component uses the @nextui-org/react input element to handle user input. This is a crucial addition to improve the user interface and user experience on our application. This change allows users to input their queries directly into the search field, providing a more interactive way to search within the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import { Card } from "@nextui-org/card";
+import SearchInput from "@/app/components/search";
+// import { Card } from "@nextui-org/card";
+// import { Search } from "@nextui-org/shared-icons";
 
 export default function Home() {
 	return (
 		<main className="dark text-foreground bg-background">
-			<section className={"container mx-auto"}>search config export </section>
+			<section className={"container mx-auto"}>
+				<SearchInput />
+				config export{" "}
+			</section>
 			<section className={"container mx-auto"}>icons</section>
-			<Card />
+			{/*<Card />*/}
 		</main>
 	);
 }
