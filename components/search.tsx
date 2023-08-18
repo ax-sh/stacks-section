a4325e241161057d36daaa5e9a268a9e874de2e9
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-08-18 15:45:35 +0545

    Refactor search component & separate SearchInput and icons
    The search component has been refactored to make it less cluttered and more readable. As part of this effort, the SearchInput functionality and the SVG for the Search

--- components/search.tsx
+++ components/search.tsx
@@ -1,85 +1,32 @@
 "use client";
-import { Input } from "@nextui-org/react";
 import type { SimpleIcon } from "simple-icons";
 import * as icons from "simple-icons";
 // import { Image } from "@nextui-org/react";
 import SVG from "react-inlinesvg";
 import dynamic from "next/dynamic";
-// const HTML5Backend: any = dynamic(
-// 	() => import("react-dnd-html5-backend").then((html) => html.HTML5Backend as any),
-// 	{ ssr: false }
-// );
+
 import { HTML5Backend } from "react-dnd-html5-backend";
 import React, { type CSSProperties, useMemo, useState } from "react";
 import { useDrag, useDrop } from "react-dnd";
+import { SearchInput } from "@/components/search-Input";
 
 const DndProvider = dynamic(async () => import("react-dnd").then((dnd) => dnd.DndProvider), {
 	ssr: false,
 });
 
-type DropResult = any
+type DropResult = any;
 
-export const SearchIcon = (props: any) => (
-	<svg
-		aria-hidden="true"
-		fill="none"
-		focusable="false"
-		height="1em"
-		role="presentation"
-		viewBox="0 0 24 24"
-		width="1em"
-		{...props}
-	>
-		<path
-			d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
-			stroke="currentColor"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			strokeWidth="2"
-		/>
-		<path
-			d="M22 22L20 20"
-			stroke="currentColor"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			strokeWidth="2"
-		/>
-	</svg>
-);
-
-function SearchInput({ setTerm, term }: { term: string; setTerm: (term: string) => void }) {
+function StackIcon({ icon }: { icon: SimpleIcon }) {
 	return (
-		<Input
-			label="Search"
-			isClearable
-			radius="lg"
-			classNames={{
-				label: "text-black/50 dark:text-white/90",
-				input: [
-					"bg-transparent",
-					"text-black/90 dark:text-white/90",
-					"placeholder:text-default-700/50 dark:placeholder:text-white/60",
-				],
-				innerWrapper: "bg-transparent",
-				inputWrapper: [
-					"shadow-xl",
-					"bg-default-200/50",
-					"dark:bg-default/60",
-					"backdrop-blur-xl",
-					"backdrop-saturate-200",
-					"hover:bg-default-200/70",
-					"dark:hover:bg-default/70",
-					"group-data-[focused=true]:bg-default-200/50",
-					"dark:group-data-[focused=true]:bg-default/60",
-					"!cursor-text",
-				],
-			}}
-			placeholder="Type to search..."
-			startContent={
-				<SearchIcon className="text-black/50 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
-			}
-			onChange={(event) => { setTerm(event.target.value); }}
-			value={term}
+		<SVG
+			src={icon.svg}
+			className={`bg-white/20 rounded-md h-20 w-20 fill-[#${icon.hex}]`}
+			// preProcessor={(code) => {
+			// 	const i = code.replace(/role=".*?"/g, `fill="#${icon.hex}"`);
+			// 	// const i = code.replace(/role=".*?"/g, `fill="currentColor"`);
+			// 	return code;
+			// }}
+			title={icon.title}
 		/>
 	);
 }
@@ -103,22 +50,10 @@ export default function IconCard({ icon }: { icon: SimpleIcon }) {
 	const opacity = isDragging ? 0.4 : 1;
 	return (
 		<div ref={drag} style={{ fill: "#" + icon.hex, opacity }}>
-			<SVG
-				key={icon.slug}
-				src={icon.svg}
-				className={`bg-white/20 rounded-md h-20 w-20 fill-[#${icon.hex}]`}
-				// preProcessor={(code) => {
-				// 	const i = code.replace(/role=".*?"/g, `fill="#${icon.hex}"`);
-				// 	// const i = code.replace(/role=".*?"/g, `fill="currentColor"`);
-				// 	console.log(i, 777);
-				// 	return code;
-				// }}
-				title={icon.title}
-			/>
+			<StackIcon key={icon.slug} icon={icon} />
 			<label>{icon.title}</label>
 		</div>
 	);
-	// return <div className={"bg-white h-10 w-10"} dangerouslySetInnerHTML={{ __html: icon.svg }} />;
 }
 
 const style: CSSProperties = {
