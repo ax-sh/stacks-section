dea01b1c0adead5127ad61ecbf1cdc1cac791939
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 01:57:25 +0545

    fix: ux

--- app/features/search/search.tsx
+++ app/features/search/search.tsx
@@ -1,16 +1,39 @@
 'use client';
 
-import React, { useState } from 'react';
-import { DndContext, DragOverlay } from '@dnd-kit/core';
+import React, { type PropsWithChildren, useState } from 'react';
+import { DndContext, DragOverlay, type UniqueIdentifier, useDroppable } from '@dnd-kit/core';
 import type { DragEndEvent } from '@dnd-kit/core/dist/types';
 import { SearchInput } from '@/app/features/search/search-input';
 import { FilteredIcons, StackIconCard } from '@/app/features/search/filtered-icons';
 import { TbDragDrop2 } from 'react-icons/tb';
 import { Example } from '@/app/features/search/example';
 import logger from '@/app/features/logger';
+import clsx from 'clsx';
 
 const child = logger.child({ type: 'search' });
-child.info('hello child!');
+child.info('search parent');
+
+export function IconDroppable(
+  props: PropsWithChildren<{ id: UniqueIdentifier; className: string }>
+) {
+  const { isOver, setNodeRef } = useDroppable({
+    id: props.id
+  });
+  const style = {
+    opacity: isOver ? 1 : 0.5
+  };
+
+  return (
+    <section
+      ref={setNodeRef}
+      style={style}
+      className={clsx(props.className, isOver && 'bg-red-400')}
+    >
+      {props.children}
+    </section>
+  );
+}
+
 export function Search() {
   const [term, setTerm] = useState('');
   const [draggedIcon, setDraggedIcon] = useState<any | null>(null);
@@ -28,11 +51,11 @@ export function Search() {
         <DragOverlay>
           {!!draggedIcon && <StackIconCard key={draggedIcon.slug} icon={draggedIcon} />}
         </DragOverlay>
-        <section className={'bg-gray-950 p-4 h-40 relative'}>
+        <IconDroppable id={'drop'} className={'bg-gray-950 p-4 h-40 relative'}>
           <div className={'absolute inset-0 grid place-content-center'}>
             <TbDragDrop2 size={64} />
           </div>
-        </section>
+        </IconDroppable>
       </DndContext>
 
       <Example />
