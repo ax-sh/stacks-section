83cfe17eff68f43706904caaffd2c867f821913e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 02:50:13 +0545

    feat: add more tests

--- store/index.test.ts
+++ store/index.test.ts
@@ -4,4 +4,11 @@ describe(useIconStore.name, () => {
   it('should load store', () => {
     expect(useIconStore.getState()).toBeDefined();
   });
+
+  it('should have empty selections', () => {
+    expect(useIconStore.getState().sections).toEqual({});
+  });
+  it('should have empty icons', () => {
+    expect(useIconStore.getState().getIcons()).toEqual([]);
+  });
 });
