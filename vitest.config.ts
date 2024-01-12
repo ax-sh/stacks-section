6f25b09d1b2f769d1af9a2d605035607727178b5
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-12 21:46:06 +0545

    feat: add vitest config and setup script

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,19 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import AutoImport from "unplugin-auto-import/vite";
 
 export default defineConfig({
-	plugins: [react(),   AutoImport({
-		imports: ['vitest'],
-		dts: true, // generate TypeScript declaration
-	}),],
+	plugins: [
+		react(),
+		AutoImport({
+			imports: ["vitest"],
+			dts: true, // generate TypeScript declaration
+		}),
+	],
 	test: {
-		environment: 'jsdom',
-		globals:true
+		setupFiles: ["./vitest-setup.ts"],
+		environment: "jsdom",
+		globals: true,
 	},
-})
+});
