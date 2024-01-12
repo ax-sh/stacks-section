43b4092e93a6fe41f1589fb2307f371672fa7019
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 00:44:53 +0545

    feat: add logo icon

--- app/features/search/search.tsx
+++ app/features/search/search.tsx
@@ -1,28 +1,12 @@
 "use client";
 
 import React, { useState } from "react";
-import { DndContext, DragOverlay, type UniqueIdentifier } from "@dnd-kit/core";
+import { DndContext, DragOverlay } from "@dnd-kit/core";
 import type { DragEndEvent } from "@dnd-kit/core/dist/types";
 import { SearchInput } from "@/app/features/search/search-input";
-import { Draggable } from "@/app/features/draggable";
-import { Droppable } from "@/app/features/droppable";
 import { FilteredIcons, StackIconCard } from "@/app/features/search/filtered-icons";
-
-function Example() {
-	const [parent, setParent] = useState<UniqueIdentifier | undefined>(undefined);
-	const draggable = <Draggable id="draggable">Go ahead, drag me.</Draggable>;
-
-	return (
-		<DndContext onDragEnd={handleDragEnd}>
-			{parent ? null : draggable}
-			<Droppable id="droppable">{parent === "droppable" ? draggable : "Drop here"}</Droppable>
-		</DndContext>
-	);
-
-	function handleDragEnd({ over }: DragEndEvent) {
-		setParent(over ? over.id : undefined);
-	}
-}
+import { TbDragDrop2 } from "react-icons/tb";
+import { Example } from "@/app/features/search/example";
 
 export function Search() {
 	const [term, setTerm] = useState("");
@@ -32,7 +16,6 @@ export function Search() {
 	}
 	function handleDragStart({ over, active, ...rest }: DragEndEvent) {
 		setDraggedIcon(active.data.current);
-		console.log("start", rest, active, "<<<<<<");
 	}
 	return (
 		<div className={"flex flex-col gap-4"}>
@@ -42,6 +25,11 @@ export function Search() {
 				<DragOverlay>
 					{!!draggedIcon && <StackIconCard key={draggedIcon.slug} icon={draggedIcon} />}
 				</DragOverlay>
+				<section className={"bg-gray-950 p-4 h-40 relative"}>
+					<div className={"absolute inset-0 grid place-content-center"}>
+						<TbDragDrop2 size={64} />
+					</div>
+				</section>
 			</DndContext>
 
 			<Example />
