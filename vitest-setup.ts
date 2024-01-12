2fb7978cfe40002109931f5e23d689bd56d85a14
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 03:14:03 +0545

    feat: add tests cleanup

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom/vitest';
+import { cleanup } from '@testing-library/react';
+
+// vi.mock('zustand'); // to make it works like `Jest` (auto-mocking)
+afterEach(() => {
+  cleanup();
+});
