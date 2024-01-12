e1988650891123dbbc7798685a759ab4854e31f5
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-12 23:33:55 +0545

    feat: add draggable icon

--- app/features/search/filtered-icons.tsx
+++ app/features/search/filtered-icons.tsx
@@ -1,30 +1,36 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { useFilteredMemoIconsList } from "@/components/search/hooks/use-filtered-memo-icons-list";
 import { IconCard } from "@/components/search/icon-card";
 import { Badge } from "@nextui-org/react";
 import { StackIcon } from "@/assets/icons";
 
+function DraggableIcon({ children }: PropsWithChildren) {
+	return <div>{children}</div>;
+}
+
 export function FilteredIcons({ term }: { term: string }) {
 	const filteredIcons = useFilteredMemoIconsList(term);
 	return (
 		<div className={"overflow-auto h-[25rem]"}>
-			<div className={"py-5 flex flex-wrap gap-2"}>
+			<div className={"flex flex-wrap gap-4"}>
 				{filteredIcons.map((icon) => {
 					const count = 0;
 					return (
-						<div
-							className={"flex flex-col items-center justify-center"}
-							style={{ fill: `#${icon.hex}` }}
-						>
-							{count ? (
-								<Badge content={count} color="primary">
+						<DraggableIcon>
+							<div
+								className={"flex flex-col items-center justify-center bg-white/10 p-4 rounded"}
+								style={{ fill: `#${icon.hex}` }}
+							>
+								{count ? (
+									<Badge content={count} color="primary">
+										<StackIcon key={icon.slug} icon={icon} />
+									</Badge>
+								) : (
 									<StackIcon key={icon.slug} icon={icon} />
-								</Badge>
-							) : (
-								<StackIcon key={icon.slug} icon={icon} />
-							)}
-							<label className={"text-xs"}>{icon.title}</label>
-						</div>
+								)}
+								<label className={"text-xs"}>{icon.title}</label>
+							</div>
+						</DraggableIcon>
 					);
 					// return <IconCard key={icon.slug} icon={icon} />;
 				})}
