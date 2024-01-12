c0dcbfd03fa8e236d50682b4b27cd6e59b72d927
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 03:48:55 +0545

    feat: add more tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: ['prettier'],
   rules: {
     'unicorn/no-array-for-each': 'off',
     'import/extensions': 'off',
@@ -7,7 +7,9 @@ module.exports = {
     'unicorn/no-array-callback-reference': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
     'no-lone-blocks': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off'
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    'n/prefer-global/process': 'off',
+    'no-implicit-coercion': 'off'
   },
   overrides: [
     {
