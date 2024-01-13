d7d8bf7849f0a8643e78c3ea469c5ee653643393
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 15:28:33 +0545

    feat: add biome ignore eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: ["next/core-web-vitals", "prettier"],
   rules: {
-    'unicorn/no-array-for-each': 'off',
-    'import/extensions': 'off',
-    'capitalized-comments': 'off',
-    'unicorn/no-array-callback-reference': 'off',
-    '@typescript-eslint/triple-slash-reference': 'off',
-    'no-lone-blocks': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    'n/prefer-global/process': 'off',
-    'no-implicit-coercion': 'off'
+    "unicorn/no-array-for-each": "off",
+    "import/extensions": "off",
+    "capitalized-comments": "off",
+    "unicorn/no-array-callback-reference": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "no-lone-blocks": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "n/prefer-global/process": "off",
+    "no-implicit-coercion": "off",
   },
   overrides: [
     {
@@ -17,19 +17,19 @@ module.exports = {
       extends: ["biome"],
     },
     {
-      files: ['**/*config.{ts,js}', 'vitest-setup.ts', '**/*.test.{tsx,jsx}'],
+      files: ["**/*config.{ts,js}", "vitest-setup.ts", "**/*.test.{tsx,jsx}"],
       rules: {
-        'unicorn/prefer-module': 'off',
-        'new-cap': 'off',
-        'import/no-unassigned-import': 'off',
-        'import/no-named-as-default': 'off'
-      }
+        "unicorn/prefer-module": "off",
+        "new-cap": "off",
+        "import/no-unassigned-import": "off",
+        "import/no-named-as-default": "off",
+      },
     },
     {
-      files: ['auto-imports.d.ts'],
+      files: ["auto-imports.d.ts"],
       rules: {
-        'unicorn/no-abusive-eslint-disable': 'off'
-      }
-    }
-  ]
+        "unicorn/no-abusive-eslint-disable": "off",
+      },
+    },
+  ],
 };
