4d871c8262bf1176cc4b6a57eef629edddf1920d
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 13:25:40 +0545

    perf: improve fetch logic

--- app/features/search/hooks/use-filtered-memo-icons-list.tsx
+++ app/features/search/hooks/use-filtered-memo-icons-list.tsx
@@ -1,19 +1,14 @@
-import { useMemo } from "react";
-import * as icons from "simple-icons";
-import { type SimpleIcon } from "simple-icons";
-import useIconStore from "@/store/icon-store";
 import logger from "@/app/features/logger";
+import useIconStore from "@/store/icon-store";
+import { useMemo } from "react";
+import { type SimpleIcon } from "simple-icons";
 
 const child = logger.child({ type: "useFilteredMemoIconsList" });
 export function useFilteredMemoIconsList(term: string) {
-
   return useMemo(() => {
     if (!term) return [];
     const predicate = (i: SimpleIcon) => i.title.toLowerCase().includes(term);
-
-    child.info("start")
-    const results =useIconStore.getState().allIcons.filter(predicate);
-    child.info("end")
+    const results = useIconStore.getState().allIcons.filter(predicate);
 
     return results;
   }, [term]);
