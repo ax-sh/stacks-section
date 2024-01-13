51559f5db23fc8d7d6a743ef2144284fda3b89df
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 13:22:04 +0545

    feat: wip

--- app/features/search/hooks/use-filtered-memo-icons-list.tsx
+++ app/features/search/hooks/use-filtered-memo-icons-list.tsx
@@ -1,8 +1,10 @@
 import { useMemo } from "react";
 import * as icons from "simple-icons";
 import { type SimpleIcon } from "simple-icons";
+import useIconStore from "@/store/icon-store";
 
 export function useFilteredMemoIconsList(term: string) {
+  // const icons = useIconStore((state) => state.allIcons)
   return useMemo(() => {
     if (!term) return [];
     const predicate = (i: SimpleIcon) => i.title.toLowerCase().includes(term);
