4daac2683ed8a219c1f63738ad09498a26ed400f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-13 12:59:01 +0545

    feat: feature/add-tailwind-scrollbar

--- app/features/search/filtered-icons.tsx
+++ app/features/search/filtered-icons.tsx
@@ -1,23 +1,23 @@
-import { DraggableIcon } from "@/app/features/draggable";
-import { useFilteredMemoIconsList } from "@/app/features/search/hooks/use-filtered-memo-icons-list";
-import { IconWithBadge } from "@/app/ui/icon-with-badge";
-import { StackIcon } from "@/components/icons";
-import useIconStore from "@/store";
-import React from "react";
-import type { SimpleIcon } from "simple-icons";
+import { DraggableIcon } from '@/app/features/draggable';
+import { useFilteredMemoIconsList } from '@/app/features/search/hooks/use-filtered-memo-icons-list';
+import { IconWithBadge } from '@/app/ui/icon-with-badge';
+import { StackIcon } from '@/components/icons';
+import useIconStore from '@/store';
+import React from 'react';
+import type { SimpleIcon } from 'simple-icons';
 
 export function StackIconCard({ icon }: Readonly<{ readonly icon: SimpleIcon }>) {
   const getSlugCount = useIconStore((state) => state.getSlugCount);
 
   return (
     <div
-      className={"flex flex-col items-center justify-center bg-white/10 p-4 gap-2 rounded"}
+      className={'flex flex-col items-center justify-center bg-white/10 p-4 gap-2 rounded'}
       style={{ fill: `#${icon.hex}` }}
     >
       <IconWithBadge content={getSlugCount(icon.slug)}>
         <StackIcon key={icon.slug} icon={icon} />
       </IconWithBadge>
-      <label className={"text-xs"}>{icon.title}</label>
+      <label className={'text-xs'}>{icon.title}</label>
     </div>
   );
 }
@@ -25,8 +25,12 @@ export function StackIconCard({ icon }: Readonly<{ readonly icon: SimpleIcon }>)
 export function FilteredIcons({ term }: { readonly term: string }) {
   const filteredIcons = useFilteredMemoIconsList(term);
   return (
-    <div className={"overflow-auto h-80"}>
-      <div className={"flex flex-wrap gap-4"}>
+    <div
+      className={
+        'overflow-auto h-80 scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100 scrollbar-thin'
+      }
+    >
+      <div className={'flex flex-wrap gap-4'}>
         {filteredIcons.map((icon) => (
           <DraggableIcon key={icon.slug} id={icon.slug} data={icon}>
             <StackIconCard icon={icon} />
