059ccb432fd878304dd7ae8e0f3b9f4ce0c76684
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-14 18:11:21 +0545

    refactor: type safety

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,24 @@
 import ExportDropdown from "@/app/ui/export-dropdown";
+import { Spacer } from "@nextui-org/react";
 import { Search } from "./features/search/search";
-import { Spacer } from '@nextui-org/react';
 
 export default function Home() {
   return (
     <main className="dark text-foreground bg-background">
       <section className={"container mx-auto"}>
-       <div className={'flex justify-end'}>
-         {/*config section*/}
-        <ExportDropdown />
-       </div>
+        <div className={"flex justify-end"}>
+          {/*
+          todo
+
+          config section
+          export to svg or html string
+          */}
+          <ExportDropdown />
+        </div>
         <Spacer y={4} />
-        {/*
-        //todo
-     
-        export to svg or html string
-        */}
+
         <Search />
       </section>
-      {/* <section className={"container mx-auto"}>icons</section> */}
     </main>
   );
 }
