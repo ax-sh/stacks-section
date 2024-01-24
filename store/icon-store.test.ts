984784791fe4f965577a1ac1a68031e05bec2a3d
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-01-24 20:39:28 +0545

    feat: add drizzle memory tests with faker

--- store/icon-store.test.ts
+++ store/icon-store.test.ts
@@ -1,61 +1,61 @@
-import useIconStore from '@/store/icon-store';
+import useIconStore from "@/store/icon-store";
 
 const initialStoreState = useIconStore.getState();
 describe(useIconStore.name, () => {
   beforeEach(() => {
     useIconStore.setState(initialStoreState, true);
   });
-  it('should load store', () => {
+  it("should load store", () => {
     expect(useIconStore.getState()).toBeDefined();
   });
 
-  it('should have empty selections', () => {
+  it("should have empty selections", () => {
     expect(useIconStore.getState().sections).toEqual({});
   });
-  it('should have empty icons', () => {
+  it("should have empty icons", () => {
     expect(useIconStore.getState().getIcons()).toEqual([]);
   });
 
-  it.todo('fix this later=>should have one selection after invoking function', async () => {
+  it.todo("fix this later=>should have one selection after invoking function", async () => {
     const state = useIconStore.getState();
-    state.addIconToSection('test');
+    state.addIconToSection("test");
 
     expect(state.sections).toEqual({ test: 1 });
 
-    state.addIconToSection('test');
-    state.addIconToSection('test');
-    state.addIconToSection('test');
-    state.addIconToSection('test');
+    state.addIconToSection("test");
+    state.addIconToSection("test");
+    state.addIconToSection("test");
+    state.addIconToSection("test");
     expect(state.sections).toEqual({ test: 5 });
   });
 
-  it('should find the number of count in selections when empty', () => {
-    expect(useIconStore.getState().getSlugCount('test')).toEqual(-1);
+  it("should find the number of count in selections when empty", () => {
+    expect(useIconStore.getState().getSlugCount("test")).toEqual(-1);
   });
 
-  it('should find the number of count in selections', () => {
+  it("should find the number of count in selections", () => {
     const state = useIconStore.getState();
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    state.addIconToSection('slug');
-    expect(useIconStore.getState().getSlugCount('slug')).toEqual(10);
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    state.addIconToSection("slug");
+    expect(useIconStore.getState().getSlugCount("slug")).toEqual(10);
   });
 
-  it('count all icons on simple icon', () => {
+  it("count all icons on simple icon", () => {
     const state = useIconStore.getState();
     expect(state.allIcons.length).toEqual(2950);
   });
 
-  it('should filter icon that matches slug', () => {
+  it("should filter icon that matches slug", () => {
     const state = useIconStore.getState();
-    expect(state.filterIconsBySlug('slug')).toEqual([]);
-    expect(state.filterIconsBySlug('fb')).toHaveLength(1);
+    expect(state.filterIconsBySlug("slug")).toEqual([]);
+    expect(state.filterIconsBySlug("fb")).toHaveLength(1);
   });
 });
