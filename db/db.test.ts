0eb1dad826c89893aab4e131f2f9eef9838c05a7
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-01-24 20:01:36 +0545

    feat: add drizzle memory tests sew

--- db/db.test.ts
+++ db/db.test.ts
@@ -1,16 +1,27 @@
 import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import * as schema from "./sqlite/schema";
 import { users } from "./sqlite/schema";
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+
+
+
+
+function buildMemoryDB() {
+  const sqlite = new Database(':memory:');
+  const db: BetterSQLite3Database<typeof schema> = drizzle(sqlite, { schema });
+
+  migrate(db, { migrationsFolder: 'drizzle' });
+  return db;
+}
+
 describe("Database memory", () => {
-  it("should load memory db", ()=>{
-    // const sqlite = new Database("sqlite.db");
-    const sqlite = new Database(":memory:");
-    const db = drizzle(sqlite, { schema });
+  it("should load memory db",async () => {
+    const db = buildMemoryDB();
+    await db.insert(users).values({ id: 'one' });
+    const user = db.select().from(users).all()
+    console.log(user);
+    expect(user).toHaveLength(1)
 
-    migrate(db, { migrationsFolder: "drizzle" });
-
-    console.log(db.select().from(users).all());
-  })
+  });
 });
